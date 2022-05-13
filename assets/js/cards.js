38fba347c179b3e7038252ea563803ef9093aaec
Yuri Cruz <yurifdev@gmail.com>
2022-05-13 13:38:56 -0300

    Adicionado a adição de novos cards usando o botão 'mais

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -1,5 +1,16 @@
-function mostraCards() {
-    document.getElementById('maisTrabalho')
+//Gets
+const adTrab = document.getElementById('maisTrabalho')
+const linha = document.querySelector('.card_linhas')
 
-}
+
+//Chamadas
+
+adTrab.addEventListener("click", function (e) {
+    const cardNovo = document.createElement("div"); // Cria um novo elemento <div>
+    cardNovo.className = "card_ad"; // Define o atributo de identificação HTML
+    cardNovo.innerHTML = "<p>Teste</p>"
+    linha.appendChild(cardNovo);
+    
+
+})
 
