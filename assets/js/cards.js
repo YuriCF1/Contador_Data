a80c74e175a21d572e1e78b843862a5fe238d62a
Yuri Cruz <yurifdev@gmail.com>
2022-05-14 18:59:37 -0300

    Corrigida a funcionalidade de cards dinâmicos pelo JS

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -3,81 +3,31 @@ const adTrab = document.getElementById('maisTrabalho')
 const linha = document.querySelector('.card_linhas')
 const inicio = document.getElementById('cards_centro');
 
-let metas = 1;
+let metas = 0;
 //Chamadas
 
 adTrab.addEventListener("click", function (e) {
     inicio.style.display = 'none';
 
-
     const cardNovo = document.createElement("div"); // Cria um novo elemento <>
     cardNovo.className = "card_ad"; // Define o atributo de identificação HTML
 
-if (metas < 6) {
-
-    if (metas == 1) {
-        
+    if (metas < 5) {
+        metas++
         cardNovo.innerHTML = "\
-        <input id='card_ad__titulo' class='card_ad__titulo' type='text' placeholder='Meta 1'>\
+        <input id='card_ad__titulo' class='card_ad__titulo' type='text' placeholder='Meta"+metas+"'>\
         <label for='meta'>O que você vai fazer?</label>\
         <textarea class='card_ad__inputs' id='meta' type='text' rows='4' cols='10' placeholder='Irei...'></textarea>\
         <label for='tempo'>Qual o prazo?</label>\
         <input class='card_ad__tempo' type='date' name='' id=''>\
-        "
-        linha.appendChild(cardNovo);
-    }
-    if (metas == 2) {
-        
-        cardNovo.innerHTML = "\
-        <input id='card_ad__titulo' class='card_ad__titulo' type='text' placeholder='Meta 2'>\
-        <label for='meta'>O que você vai fazer?</label>\
-        <textarea class='card_ad__inputs' id='meta' type='text' rows='4' cols='10' placeholder='Irei...'></textarea>\
-        <label for='tempo'>Qual o prazo?</label>\
-        <input class='card_ad__tempo' type='date' name='' id=''>\
-        "
-        linha.appendChild(cardNovo);
-    }
-    if (metas == 3) {
-        
-        cardNovo.innerHTML = "\
-        <input id='card_ad__titulo' class='card_ad__titulo' type='text' placeholder='Meta 3'>\
-        <label for='meta'>O que você vai fazer?</label>\
-        <textarea class='card_ad__inputs' id='meta' type='text' rows='4' cols='10' placeholder='Irei...'></textarea>\
-        <label for='tempo'>Qual o prazo?</label>\
-        <input class='card_ad__tempo' type='date' name='' id=''>\
-        "
-        linha.appendChild(cardNovo);
-    }
-
-    if (metas == 4) {
-        
-        cardNovo.innerHTML = "\
-        <input id='card_ad__titulo' class='card_ad__titulo' type='text' placeholder='Meta 4'>\
-        <label for='meta'>O que você vai fazer?</label>\
-        <textarea class='card_ad__inputs' id='meta' type='text' rows='4' cols='10' placeholder='Irei...'></textarea>\
-        <label for='tempo'>Qual o prazo?</label>\
-        <input class='card_ad__tempo' type='date' name='' id=''>\
-        "
-
-        linha.appendChild(cardNovo);
-    }    
-
-    if (metas == 5) {
-
-        cardNovo.innerHTML = "\
-        <input id='card_ad__titulo' class='card_ad__titulo' type='text' placeholder='Meta 5 '>\
-        <label for='meta'>O que você vai fazer?</label>\
-        <textarea class='card_ad__inputs' id='meta' type='text' rows='4' cols='10' placeholder='Irei...'></textarea>\
-        <label for='tempo'>Qual o prazo?</label>\
-        <input class='card_ad__tempo' type='date' name='' id=''>\
-        "
-
-        linha.appendChild(cardNovo);
-    }    
+       "
+    linha.appendChild(cardNovo);
     
-}
-    
-metas++
+    } else {
+        alert('Foque nas 5')
+
+    }
+
 
 })
 
