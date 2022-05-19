693d11f010982c7e18e9c29d3f28a67f1a86ea84
Yuri Cruz <yurifdev@gmail.com>
2022-05-18 23:38:47 -0300

    Card adicionado alterado

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -15,11 +15,18 @@ adTrab.addEventListener("click", function (e) {
     if (metas < 5) {
         metas++
         cardNovo.innerHTML = "\
-        <input id='card_ad__titulo' class='card_ad__titulo' type='text' placeholder='Meta"+metas+"'>\
-        <label for='meta'>O que você vai fazer?</label>\
-        <textarea class='card_ad__inputs' id='meta' type='text' rows='4' cols='10' placeholder='Irei...'></textarea>\
-        <label for='tempo'>Qual o prazo?</label>\
-        <input class='card_ad__tempo' type='date' name='' id=''>\
+        <input id='card_ad__titulo' class='card_ad__titulo' type='text' placeholder='Meta "+metas+"'>\
+                    <label for='meta'>O que você vai fazer?</label>\
+                    <textarea class='card_ad__inputs' id='meta' type='text' rows='4' cols='10'\
+                        placeholder='Irei...'></textarea>\
+                    <label for='tempo'>Qual o prazo?</label>\
+                        <input class='card_ad__tempo' type='datetime-local' name='' id='r-data'>\
+                        <input class='botao' type='submit' value='Começar!' id='botao' name='botao'>\
+                    <div><time id='f_dias'></time>\
+                        <time id='f_horas'></time>\
+                        <time id='f_minutos'></time>\
+                        <time id='f_segundos'></time>\
+                    </div>\
        "
     linha.appendChild(cardNovo);
 
