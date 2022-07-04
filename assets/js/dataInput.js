a3190442364979a5f23653dd0fa9a49703a076a7
Yuri Cruz <99689063+YuriCF1@users.noreply.github.com>
2022-07-04 20:47:20 -0300

    Organizando código. PC em concerto

--- assets/js/dataInput.js
+++ assets/js/dataInput.js
@@ -1,4 +1,4 @@
-//ADICIONANDO INPUT
+//ADDING THE INPUT USING A MINIMUM TIME 
 const inputDataLocal = document.getElementById('data');
 let min = inputDataLocal.min = new Date().toISOString().slice(0, 16); //Pegando a string da data, mas até os mínutos
 // let min1 = inputDataLocal.min = new Date().toISOString().split(".")[0];
